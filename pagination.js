bdb860a3c3310618ac2a5bcc86d7d3b718328ec5
katarina-andrews <andrews.katarina@gmail.com>
2025-06-29 12:18:40 -0500

    changed html structure added styling and added finished functions

--- pagination.js
+++ pagination.js
@@ -1,11 +1,12 @@
 const state = {
   currentPage: 1,
   currentPosts: "",
+  isRateLimit: false,
 };
 
+async function fetchPosts(page) {
+  state.isRateLimit = true;
 
-async function fetchPosts(page, posts) {
- state.currentPosts = posts;
   try {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
@@ -22,11 +23,89 @@ async function fetchPosts(page, posts) {
   } finally {
     console.log("Finished fetching by posts");
   }
- }
+}
 
- fetchPosts(1)
- fetchPosts(2)
+function renderPosts(posts) {
+  state.currentPosts = posts;
+  const postsContainer = document.getElementById("posts-container");
+  postsContainer.innerHTML = "";
 
-// function renderPosts(posts) {
+  posts.forEach((posts) => {
+    const postsElm = document.createElement("div");
+    postsElm.className = "border rounded-sm p-3 bg-amber-50 text-amber-500";
 
-// }
+    const UserElm = document.createElement("p");
+    UserElm.innerHTML = posts.userId;
+    UserElm.className = "font-bold";
+
+    const idElm = document.createElement("p");
+    idElm.innerHTML = posts.id;
+    idElm.className = "font-bold";
+
+    const titleElm = document.createElement("p");
+    titleElm.innerHTML = posts.title;
+
+    const bodyElm = document.createElement("p");
+    bodyElm.innerHTML = posts.body;
+
+    postsElm.appendChild(UserElm);
+    postsElm.appendChild(idElm);
+    postsElm.appendChild(titleElm);
+    postsElm.appendChild(bodyElm);
+
+    postsContainer.appendChild(postsElm);
+  });
+}
+
+function renderPagination() {
+  const totalPages = 10; // there are 100 total posts in the API
+
+  const paginationContainer = document.getElementById("pagination-container");
+  paginationContainer.innerHTML = "";
+  const prevBtn = document.getElementById("previous");
+  const nextBtn = document.getElementById("next");
+
+  prevBtn.disabled = state.currentPage === 1;
+  nextBtn.disabled = state.currentPage === totalPages;
+
+  //previous button doesn't work on first click for some reason
+  prevBtn.onclick = async () => {
+    if (state.isRateLimit) {
+      alert("Rate limit exceeded. Please wait.");
+      return;
+    }
+
+    state.isRateLimit = true;
+    state.currentPage--;
+
+    const data = await fetchPosts(state.currentPage);
+    renderPosts(data);
+    renderPagination();
+
+    setTimeout(() => (state.isRateLimit = false), 2000);
+  };
+
+  nextBtn.onclick = async () => {
+    if (state.isRateLimit) {
+      alert("Rate limit exceeded. Please wait.");
+      return;
+    }
+
+    state.isRateLimit = true;
+
+    const data = await fetchPosts(state.currentPage);
+    renderPosts(data);
+    renderPagination();
+
+    state.currentPage++;
+
+    setTimeout(() => (state.isRateLimit = false), 2000);
+  };
+
+  const pageCountElem = document.createElement("p");
+  pageCountElem.innerHTML = `Page ${state.currentPage}`; //can't figure out how to get rid on the "Page 1" showing up initially
+  pageCountElem.className = "text-center font-bold text-amber-500";
+  paginationContainer.appendChild(pageCountElem);
+}
+
+renderPagination();
